76a237ddc6fcf952a379fd1d320581e06016cb79
TGOlson <tydotg@gmail.com>
2013-11-19 23:25:50 -0800

    first test for routes and controller workign -- Todo class in working order

---
+++ spec/features/todopage_spec.rb
@@ -0,0 +1,12 @@
+# require 'spec_helper'
+
+# describe 'Todo Page' do
+# 	context 'Can click a button to create a new todo' do
+# 		visit '/todos'
+# 		page.should have_content 'Create a new todo'
+# 	end
+
+
+# end
+
+### VISIT IS BROKEN
