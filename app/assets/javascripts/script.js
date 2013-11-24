94f878f63bb8b703c535f0dfb9ceeb4c91e21bad
TGOlson <tydotg@gmail.com>
2013-11-24 13:23:01 -0800

    test todo and action widths

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -9,7 +9,7 @@ $(function(){
 	$('#create-new').on('ajax:success', function(e, data, status, xhr){
 		// uniqId helps preserve state if multiple new forms tabs are opened
 		var uniqId = Math.floor(Math.random()*10000)
-		$('#todos').append('<li class="single-todo unsaved" id="' + uniqId + '">' + data + '</li>')
+		$('#todos').append('<li class="single-todo unsaved" id="' + uniqId + '">' + data + '</li>').fadeIn()
 		checkForQuickDelete()
 		checkForSubmit(uniqId)
 	})
@@ -18,7 +18,7 @@ $(function(){
 
 function checkForQuickDelete(){
 	$('.unsaved i').on('click',function(){
-		$(this).closest('li').remove()
+		$(this).closest('li').fadeOut(250)
 	})
 }
 
