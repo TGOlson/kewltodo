d737617cda99f96e7a5e180d55ec99a7f1ff8747
TGOlson <tydotg@gmail.com>
2013-11-24 00:29:45 -0800

    add new styling and ajax

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,4 +1,16 @@
 $(function(){
 	$('#todos').sortable()
 	$('#lists').sortable()
+
+	$('#create-new').on('ajax:success', function(e, data, status, xhr){
+		$('#todos').append('<li class="single-todo unsaved">' + data + '</li>')
+		checkForQuickDelete()
+	})
 })
+
+
+function checkForQuickDelete(){
+	$('.unsaved').on('click',function(){
+		$(this).remove()
+	})
+}
